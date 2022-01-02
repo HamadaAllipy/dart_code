166922bb16ccd1f13afc24c6ef02f38e87591f93
hamada <hamada.devlop@gmail.com>
2022-01-02 05:16:49 +0200

    (solid)2-open_closed

--- bin/examples/solid/2-open_closed/ex_1/bad/bad.dart
+++ bin/examples/solid/2-open_closed/ex_1/bad/bad.dart
@@ -1,20 +1,22 @@
 class Rectangle {
-  final double? _width;
-  final double? _height;
+  final double _width;
+  final double _height;
 
   Rectangle(double width, double height)
       : _height = height,
         _width = width;
 
-  double? get width => _width;
+  double get width => _width;
 
-  double? get height => _height;
+  double get height => _height;
 }
 
 class Square {
   final double _side;
 
   Square(double side) : _side = side;
+
+  double get side=> _side;
 }
 
 class ShapePrinter {
